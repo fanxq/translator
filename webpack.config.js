a5412377ecc3310ddcd5784460c3559d3c554df9
fan <1508422424@qq.com>
2018-11-05 22:01:33 +0800

    更新webpack配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/main.js',
     output: {
       path: path.resolve(__dirname, 'out'),
