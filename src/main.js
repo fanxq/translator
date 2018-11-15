783464bd7ee792b561f04f893cc199c8e1f9746d
fan <1508422424@qq.com>
2018-11-15 23:25:31 +0800

    使用shadow dom承载翻译内容

--- src/main.js
+++ src/main.js
@@ -4,7 +4,39 @@ let translationOutput = document.createElement('DIV');
 translationOutput.style.display = 'none';
 document.addEventListener("DOMContentLoaded",function(){
     document.body.appendChild(translationOutput);
+    let shadow = translationOutput.attachShadow({mode:'open'});
+    let style = document.createElement("style");
+    style.textContent = `
+        .popup{
+            font-size:14px;
+            padding:5px 5px;
+            position:relative;
+            border:1px solid rgba(0, 0, 0, 0.2);
+            box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.35);
+            background-color: white;
+        }
+        .popup::after{
+            content:'';
+            position:absolute;
+            left: 6px;
+            top: -6px;
+            width:10px;
+            height:10px;
+            transform: rotate(45deg);
+            background-color: white;
+            border-top:1px solid rgba(0, 0, 0, 0.2);
+            border-left:1px solid rgba(0, 0, 0, 0.2);
+        }
+    `;
+    shadow.appendChild(style);
+    let output = document.createElement('div');
+    output.setAttribute('class','popup');
+    shadow.appendChild(output);
     document.addEventListener("mousedown",function(e){
+        if(e.currentTarget == translationOutput){
+            console.log('hit output');
+            return;
+        }
         if(translationOutput){
             if(translationOutput.style.display !== 'none'){
                 translationOutput.style.display = 'none';
@@ -12,6 +44,10 @@ document.addEventListener("DOMContentLoaded",function(){
         }
     });
     document.addEventListener("mouseup",function(e){
+        if(e.currentTarget == translationOutput){
+            console.log('hit output');
+            return;
+        }
         new Promise(function(resolve,reject){
             chrome.storage.local.get({'enable':true}, function(result){
                 resolve(result);
@@ -32,21 +68,17 @@ document.addEventListener("DOMContentLoaded",function(){
                         translationOutput.style.display = 'block';
                         translationOutput.style.position = 'absolute';
                         translationOutput.style.zIndex = '2147483647';
-                        translationOutput.style.backgroundColor = 'white';
-                        translationOutput.style.border = '1px solid rgba(0, 0, 0, 0.2)';
-                        translationOutput.style.padding = '5px 5px';
-                        translationOutput.style.top = tPoint.y - 40+'px';
+                        translationOutput.style.top = tPoint.y + 10 + 'px';
                         translationOutput.style.left = tPoint.x+'px';
-                        translationOutput.style.boxShadow = '3px 3px 3px rgba(0, 0, 0, 0.35)';
-                        translationOutput.innerText = "翻译中......";
+                        output.innerText = "翻译中......";
                         translator.doTranslate(selectedText).then(function(result){
-                            if(result){
-                                translationOutput.innerText = result;
+                            if(result){ 
+                                output.innerText  = result;
                             }else{
-                                translationOutput.innerText = '翻译出错了！';
+                                output.innerText = '翻译出错了！';
                             }
                         }).catch(function(e){
-                            translationOutput.innerText = '翻译出错了！';
+                            output.innerText = '翻译出错了！';
                         });
                     }
                 }
