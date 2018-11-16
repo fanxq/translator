f7f668cb84f9a64f3d502dbcb26c356636e1071b
fan <1508422424@qq.com>
2018-11-16 23:18:40 +0800

    调整翻译结果显示的位置

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import {Translate} from './translate.js';
+let startX = 0; let startY = 0;
+let endX = 0; let endY = 0;
+let selectedText;
+let isMouseDown = false;
+let isSelected = false;
 let translator = new Translate();
 let translationOutput = document.createElement('DIV');
 translationOutput.style.display = 'none';
@@ -33,21 +38,46 @@ document.addEventListener("DOMContentLoaded",function(){
     output.setAttribute('class','popup');
     shadow.appendChild(output);
     document.addEventListener("mousedown",function(e){
-        if(e.currentTarget == translationOutput){
-            console.log('hit output');
-            return;
-        }
         if(translationOutput){
             if(translationOutput.style.display !== 'none'){
                 translationOutput.style.display = 'none';
             }
         }
+        isMouseDown = true;
     });
+    document.addEventListener('mousemove',function(e){
+        if(isMouseDown){
+            var selObj = window.getSelection();
+            if(!isSelected && selObj){
+                endX = startX = e.pageX;
+                endY = startY = e.pageY;
+                isSelected = true;
+                selectedText = selObj.toString();
+                return;
+            }
+            if(isSelected && selObj){
+                if(selObj.toString() === selectedText){
+                    return;
+                }
+                endX = e.pageX;
+                endY = e.pageY;
+                selectedText = selObj.toString();
+                console.log("x:",endX,"y2:",endY,"text:",selectedText);
+            }
+        }
+    },false);
     document.addEventListener("mouseup",function(e){
-        if(e.currentTarget == translationOutput){
-            console.log('hit output');
+        if(!isSelected){
             return;
         }
+        let fontSize = 14;
+        try{ 
+			fontSize = window.getComputedStyle(window.getSelection().anchorNode.nodeType == 3?window.getSelection().anchorNode.parentElement:window.getSelection().anchorNode).fontSize;
+		}catch(e){
+			console.log(e);
+        }
+        var left = endX - (endX - startX)/2 - 6;
+		var top = Math.max(endY, startY) + parseInt(fontSize);
         new Promise(function(resolve,reject){
             chrome.storage.local.get({'enable':true}, function(result){
                 resolve(result);
@@ -56,10 +86,10 @@ document.addEventListener("DOMContentLoaded",function(){
             if(!result.enable){
                 return;
             }
-            var tPoint = {
-                x:e.pageX,
-                y:e.pageY
-            }
+            // var tPoint = {
+            //     x:e.pageX,
+            //     y:e.pageY
+            // }
             var selection = window.getSelection();
             if(selection){
                 var selectedText = selection.toString();
@@ -68,8 +98,10 @@ document.addEventListener("DOMContentLoaded",function(){
                         translationOutput.style.display = 'block';
                         translationOutput.style.position = 'absolute';
                         translationOutput.style.zIndex = '2147483647';
-                        translationOutput.style.top = tPoint.y + 10 + 'px';
-                        translationOutput.style.left = tPoint.x+'px';
+                        // translationOutput.style.top = tPoint.y + 10 + 'px';
+                        // translationOutput.style.left = tPoint.x+'px';
+                        translationOutput.style.top = top + 'px';
+                        translationOutput.style.left = left + 'px';
                         output.innerText = "翻译中......";
                         translator.doTranslate(selectedText).then(function(result){
                             if(result){ 
@@ -83,6 +115,10 @@ document.addEventListener("DOMContentLoaded",function(){
                     }
                 }
             }
-        }); 
+        });
+        isMouseDown = false;
+        selectedText = '';
+        isSelected = false;
+        //startX = startY = endX = endY = 0; 
     });
 });
