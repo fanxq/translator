7d5bf41b4d7a778cff718dbbd38d3cd4098661f4
fan <1508422424@qq.com>
2018-12-19 23:39:53 +0800

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 var gulp = require('gulp');
 
-gulp.task('default', function() {
+gulp.task('default', function (done) {
     gulp.src([
-        './src/background.js',
-        './src/manifest.json',
-        './src/images/icon16.png',
-        './src/images/icon48.png',
-        './src/images/icon128.png',
-        './src/popup.js',
-        './src/html/popup.html'
-    ])
-    .pipe(gulp.dest('out'));
+            './src/manifest.json',
+            './src/images/icon16.png',
+            './src/images/icon48.png',
+            './src/images/icon128.png',
+            './src/popup.js',
+            './src/html/popup.html'
+        ])
+        .pipe(gulp.dest('out'));
+    done();
 });
