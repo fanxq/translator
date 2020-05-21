8cf91a9810524e778287148e697e87b0546bacd3
fanxq <1508422424@qq.com>
2020-05-21 14:54:51 +0800

    1.update style; 2.rename folder 'html' to 'page'; 3.update functions to set the position of the widget

--- src/extension.js
+++ src/extension.js
@@ -29,7 +29,8 @@ class TranslationExtension {
   constructor() {
     this.startPoint = new Point(0, 0);
     this.endPoint = new Point(0, 0);
-    // this.selectedText = '';
+    this.selectedText = '';
+    this.isSelected = false;
     this.isMousedown = false;
     this.widget = null;
     this.widgetContent = null;
@@ -40,12 +41,16 @@ class TranslationExtension {
     this.initWidget();
     this.watchChromeRuntimeMessage();
     this.watchMousedownOnDocument();
+    this.watchMousemoveOnDocument();
     this.watchMouseupOnDocument();
   }
 
   initWidget() {
     this.widget = document.createElement('div');
     this.widget.style.display = 'none';
+    this.widget.style.backgroundColor = '#fff';
+    this.widget.style.boxShadow = '0 2px 10px 0 rgba(0, 0, 0, 0.2)';
+    this.widget.style.borderRadius = '5px';
     let shadowRoot = this.widget.attachShadow({
       mode: 'open'
     });
@@ -53,7 +58,7 @@ class TranslationExtension {
     style.textContent = extensionStyle.toString();
     shadowRoot.appendChild(style);
     this.widgetContent = document.createElement('div');
-    this.widgetContent.setAttribute('class', 'popup');
+    this.widgetContent.setAttribute('class', 'content');
     shadowRoot.appendChild(this.widgetContent);
     document.body.appendChild(this.widget);
   }
@@ -84,20 +89,34 @@ class TranslationExtension {
   watchMousedownOnDocument() {
     document.addEventListener('mousedown', (e) => {
       this.hideWidget();
-      this.startPoint.x = e.pageX;
-      this.startPoint.y = e.pageY;
-      // this.isMousedown = true;
+      this.isMousedown = true;
     });
   }
 
-  // watchMousemoveOnDocument() {
-  //   document.addEventListener('mousemove', (e) => {
-  //     if (this.isMousedown) {
-  //       this.endPoint.x = e.pageX;
-  //       this.endPoint.y = e.pageY;
-  //     }
-  //   });
-  // }
+  watchMousemoveOnDocument() {
+    document.addEventListener('mousemove', (e) => {
+      if (this.isMousedown) {
+       
+        let selection = window.getSelection();
+        let selectedText = selection ? selection.toString() : '';
+        if (!this.isSelected && selectedText) {
+          this.startPoint.x = e.pageX;
+          this.startPoint.y = e.pageY;
+          this.endPoint.x = e.pageX;
+          this.endPoint.y = e.pageY;
+          this.isSelected = true;
+          this.selectedText = selectedText;
+        }
+        if (this.isSelected && selectedText) {
+          if (selectedText === this.selectedText) {
+            return;
+          }
+          this.endPoint.x = e.pageX;
+          this.endPoint.y = e.pageY;
+        }
+      }
+    });
+  }
 
   getOffsetToBody(element) {
     let rect = element.getBoundingClientRect();
@@ -111,44 +130,46 @@ class TranslationExtension {
 
   watchMouseupOnDocument() {
     document.addEventListener('mouseup', (e) => {
-      // if (this.isMousedown) {
-      //   this.endPoint.x = e.pageX;
-      //   this.endPoint.y = e.pageY;
-      let selection = window.getSelection();
-      let selectedText = selection ? selection.toString() : '';
-      if (selectedText) {
-        new Promise((resolve, reject) => {
-          chrome.storage.local.get({
-            'enable': true
-          }, function (result) {
-            resolve(result);
-          });
-        }).then(result => {
-          if (!result.enable) {
-            return;
-          }
-          let anchorOffsetToBody = this.getOffsetToBody(selection.anchorNode.nodeType === Node.TEXT_NODE ? selection.anchorNode.parentNode : selection.anchorNode);
-          let focusOffsetToBody = this.getOffsetToBody(selection.focusNode.nodeType === Node.TEXT_NODE ? selection.focusNode.parentNode : selection.focusNode);
-          let anchorPos = new Point(anchorOffsetToBody.left, anchorOffsetToBody.bottom);
-          let focusPos = new Point(focusOffsetToBody.left, focusOffsetToBody.bottom);
-          let pos = Point.max(anchorPos, focusPos);
-          this.showWidget({
-            left: pos.x,
-            top: pos.y
-          });
-
-          chrome.runtime.sendMessage({
-            action: 'translate',
-            text: selectedText.trim()
-          });
-        }).then(() => {
-          this.startPoint = new Point(0, 0);
-          this.endPoint = new Point(0, 0);
-        });
+      if (!this.isSelected) {
+        return;
       }
-      // }
-      // this.isMousedown = false;
+      let left = this.endPoint.x - (this.endPoint.x -this.startPoint.x) / 2 - 6;
+      new Promise((resolve, reject) => {
+        chrome.storage.local.get({
+          'enable': true
+        }, function (result) {
+          resolve(result);
+        });
+      }).then(result => {
+        if (!result.enable) {
+          return;
+        }
+        let selection = window.getSelection();
+        let selectedText = selection ? selection.toString().trim() : '';
+        if (!selectedText) {
+          return;
+        }
+        let anchorOffsetToBody = this.getOffsetToBody(selection.anchorNode.nodeType === Node.TEXT_NODE ? selection.anchorNode.parentNode : selection.anchorNode);
+        let focusOffsetToBody = this.getOffsetToBody(selection.focusNode.nodeType === Node.TEXT_NODE ? selection.focusNode.parentNode : selection.focusNode);
+        let anchorPos = new Point(anchorOffsetToBody.left, anchorOffsetToBody.bottom);
+        let focusPos = new Point(focusOffsetToBody.left, focusOffsetToBody.bottom);
+        let pos = Point.max(anchorPos, focusPos);
+        this.showWidget({
+          left: left,
+          top: pos.y + 10
+        });
 
+        chrome.runtime.sendMessage({
+          action: 'translate',
+          text: selectedText
+        });
+      }).then(() => {
+        this.startPoint = new Point(0, 0);
+        this.endPoint = new Point(0, 0);
+        this.isMousedown = false;
+        this.isSelected = false;
+        this.selectedText = '';
+      });
     });
   }
 }
