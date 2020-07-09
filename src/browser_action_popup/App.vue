02dc6658cc2c65df7e37a945217c7c84b7b94d14
fanxq <1508422424@qq.com>
2020-07-09 18:39:00 +0800

    update

--- src/browser_action_popup/App.vue
+++ src/browser_action_popup/App.vue
@@ -8,6 +8,32 @@
       <span>启用截图翻译</span>
       <switch-button v-model="enableScreenshot" :disabled="!enable"/>
     </div>
+    <div v-show="enableScreenshot" class="option-item">
+      <span>识别为：</span>
+      <ul>
+        <li 
+          v-for="item in langs" 
+          :key="item.code" 
+          :class="{active: recognizeTo === item.code}"
+          @click="setRecognizeLang(item.code)"
+        >
+          {{item.name}}
+        </li>
+      </ul>
+    </div>
+    <div v-show="enableScreenshot" class="option-item">
+      <span>翻译为：</span>
+      <ul>
+        <li 
+          v-for="item in langs" 
+          :key="item.code" 
+          :class="{active: recognizeTo === item.code}"
+          @click="setRecognizeLang(item.code)"
+        >
+          {{item.name}}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -21,6 +47,13 @@ export default {
     return {
       enable: false,
       enableScreenshot: false,
+      langs: [
+        {name: '简体中文', code: 'chi_sim'},
+        {name: '英文', code: 'eng'},
+        {name: '日文', code: 'jpn'}
+      ],
+      recognizeTo: 'eng',
+      translateTo: 'zh'
     }
   },
   watch: {
@@ -41,6 +74,15 @@ export default {
     chrome.storage.local.get('enableScreenshot', (reslut) => {
       this.enableScreenshot = reslut.enableScreenshot || false;
     });
+    chrome.storage.local.get('recognizeTo', (result) => {
+      this.recognizeTo = result.recognizeTo || 'eng';
+    });
+  },
+  methods: {
+    setRecognizeLang(code) {
+      this.recognizeTo = code;
+      chrome.storage.local.set({'recognizeTo': code});
+    }
   }
 }
 </script>
@@ -50,12 +92,29 @@ export default {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    width: 230px;
+    width: 280px;
     font-size: 16px;
     font-family: sans-serif;
     padding: 8px;
     &:first-child {
       border-bottom: 1px solid #ccc;
+    }
+    ul {
+      list-style: none;
+      padding: 0px;
+      margin: 0px;
+      li {
+        display: inline-block;
+        margin: 0px 5px;
+        padding: 5px;
+        border-radius: 2px;
+        cursor: pointer;
+        border: 1px solid #4b8bf4;
+        &.active {
+          background-color: #4b8bf4;
+          color: #fff;
+        }
+      }
     }
   }
 </style>
