cda0d5210e64cef2711eac654a9205dc9441420a
fan <1508422424@qq.com>
2020-12-14 16:31:25 +0800

    抽取webpack共用配置，添加terser-webpack-plugin实现生产模式下打包去除console

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,63 +1,10 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { merge } = require('webpack-merge');
 const WebpackChromeReloaderPlugin = require('webpack-chrome-extension-reloader');
+const baseConfig = require('./webpack.config.base');
 
-module.exports = {
+module.exports = merge(baseConfig, {
   mode: 'development',
-  entry: {
-    main: './src/content_scripts/main.js',
-    background: './src/background/background.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'out'),
-    filename: '[name].js'
-  },
-  module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'style-loader',
-            options: {
-              injectType: 'lazyStyleTag',
-              insert: function insertAtTop(element) {
-                var parent = document.querySelector('#TX_SH_0001').shadowRoot;
-                // eslint-disable-next-line no-underscore-dangle
-                var lastInsertedElement =
-                  window._lastElementInsertedByStyleLoader;
-
-                if (!lastInsertedElement) {
-                  parent.insertBefore(element, parent.firstChild);
-                } else if (lastInsertedElement.nextSibling) {
-                  parent.insertBefore(element, lastInsertedElement.nextSibling);
-                } else {
-                  parent.appendChild(element);
-                }
-
-                // eslint-disable-next-line no-underscore-dangle
-                window._lastElementInsertedByStyleLoader = element;
-              }
-            }
-          },
-          'css-loader',
-          'sass-loader'
-        ],
-      }
-    ]
-  },
   plugins: [
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, './src/manifest.json'),
-        to: path.resolve(__dirname, './out/manifest.json')
-      }, {
-        from: path.resolve(__dirname, './src/assets'),
-        to: path.resolve(__dirname, './out'),
-        ignore: ['images/*.gif', 'config/**/*', 'scss/**/*']
-      }
-    ]),
     new WebpackChromeReloaderPlugin({
       port: 9091,
       reloadPage: true,
@@ -67,4 +14,4 @@ module.exports = {
       }
     }),
   ]
-}
+}); 
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,50 +1,20 @@
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { merge } = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const baseConfig = require('./webpack.config.base');
 
-module.exports = {
+module.exports = merge(baseConfig, {
   mode: 'production',
-  entry: {
-    main: './src/content_scripts/main.js',
-    background: './src/background/background.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'out'),
-    filename: '[name].js'
-  },
-  module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'style-loader',
-            options: {
-              injectType: 'lazyStyleTag',
-              insert: function insertAtTop(element) {
-                var parent = document.querySelector('#TX_SH_0001').shadowRoot;
-                // eslint-disable-next-line no-underscore-dangle
-                var lastInsertedElement =
-                  window._lastElementInsertedByStyleLoader;
-
-                if (!lastInsertedElement) {
-                  parent.insertBefore(element, parent.firstChild);
-                } else if (lastInsertedElement.nextSibling) {
-                  parent.insertBefore(element, lastInsertedElement.nextSibling);
-                } else {
-                  parent.appendChild(element);
-                }
-
-                // eslint-disable-next-line no-underscore-dangle
-                window._lastElementInsertedByStyleLoader = element;
-              }
-            }
-          },
-          'css-loader',
-          'sass-loader'
-        ],
-      }
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true
+          }
+        }
+      })
     ]
   },
   plugins: [
@@ -52,15 +22,5 @@ module.exports = {
       verbose: true,
       cleanOnceBeforeBuildPatterns: [path.resolve(__dirname, './out/**/*')],
     }),
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, './src/manifest.json'),
-        to: path.resolve(__dirname, './out/manifest.json')
-      }, {
-        from: path.resolve(__dirname, './src/assets'),
-        to: path.resolve(__dirname, './out'),
-        ignore: ['images/*.gif', 'config/**/*', 'scss/**/*']
-      }
-    ])
   ]
-}
+});
